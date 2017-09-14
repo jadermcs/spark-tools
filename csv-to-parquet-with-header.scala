87afc41824383b5ecae4b20a5d9ecf167baf0b56
Jader Martins <jadermcs@users.noreply.github.com>
2017-09-14 16:20:46 -0300

    Update csv-to-parquet-with-header.scala

--- csv-to-parquet-with-header.scala
+++ csv-to-parquet-with-header.scala
@@ -1,15 +1,28 @@
-def main(args: Array[String]): Unit = {
-      //recebe arquivo.csv do argparser
-      val filein = args[0]
-      //recebe nome do folder a ser criado para salvar o parquet do argparser
-      val fileout = args[1] 
-      
-      def convert(filename: String, outparquet: String): Unit = {
-            //le csv definindo o cabecalho
-            val df = spark.read.option("header","true").csv(filename)
-            //salva para parquet
-            df.write.parquet(outparquet)
-        }
+// scalastyle:off println
 
-      convert(filein, fileout)
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object Csv2Parquet {
+      def main(args: Array[String]): Unit = {
+            //recebe arquivo.csv do argparser
+            val filein = args[0]
+            //recebe nome do folder a ser criado para salvar o parquet do argparser
+            val fileout = args[1]
+
+            val conf = new SparkConf().setMaster("local[*]").setAppName("csv2parquet")
+            val sc = new SparkContext(conf)
+
+            def convert(filename: String, outparquet: String): Unit = {
+                  //le csv definindo o cabecalho
+                  val df = sc.read.option("header","true").csv(filename)
+                  //salva para parquet
+                  df.write.parquet(outparquet)
+              }
+
+            convert(filein, fileout)
+      }
 }
+      
+// scalastyle:on println
